1c5040ee86a237d4d0d8f2fc5ea6c9cd6f1fb946
MBAMER <163824331+MBAMER@users.noreply.github.com>
2025-02-28 10:56:52 +0100

    Feat(authcontroller)
    J'ai rajouté le salt et j'ai hacher le password

--- nodejswebshop/app/controllers/AuthController.js
+++ nodejswebshop/app/controllers/AuthController.js
@@ -1,4 +1,5 @@
 const mysql = require("mysql2/promise");
+const crypto = require("crypto");
 const connection = mysql.createPool({
   host: "db_container",
   user: "root",
@@ -6,6 +7,18 @@ const connection = mysql.createPool({
   port: 3306,
   database: "db_users",
 });
+
+const generateSalt = () => {
+  return crypto.randomBytes(8).toString("hex");
+};
+
+// Fonction pour hacher le mot de passe avec un salt
+const hashPassword = (password, salt) => {
+  const hash = crypto.createHmac("sha256", salt);
+  hash.update(password);
+  return hash.digest("hex");
+};
+
 module.exports = {
   get: (req, res) => {
     res.render("login", { name: "Mathieu" });
@@ -17,17 +30,18 @@ module.exports = {
 
   createUser: (req, res) => {
     console.log(req.body);
-    //const status = connection.connect();
-
-    //console.log("status de la connexion : " + status);
 
     try {
       // For pool initialization, see above
       const { username, password } = req.body;
 
+      const salt = generateSalt();
+      const hashedPassword = hashPassword(password, salt);
+      console.log(hashedPassword);
+
       connection.query(
-        "INSERT INTO `t_user`(`username`, `password`) VALUES (?,?)",
-        [username, password]
+        "INSERT INTO `t_user`(`username`, `password`, `salt`) VALUES (?,?,?)",
+        [username, hashedPassword, salt]
       );
       // Connection is automatically released when query resolves
     } catch (err) {
